44ed1729b1d324453ed5da15c4c9ab4d7617a428
Rajheem O'Connor <rajheem1@gmail.com>
2020-02-23 22:28:32 -0500

    'Lab4'

---
+++ app/forms.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+class UploadForm(FlaskForm):
+    upload = FileField(validators=[
+        FileRequired(),FileAllowed(['jpg', 'png'], 'Images only!')])
--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ import os
 from app import app
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from werkzeug.utils import secure_filename
+from app.forms import UploadForm
 
 
 ###
@@ -25,22 +26,24 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
-
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
     if not session.get('logged_in'):
         abort(401)
 
     # Instantiate your form class
-
+    form=UploadForm()
     # Validate file upload on submit
+    
     if request.method == 'POST':
+        if form.validate_on_submit():
+            f = form.upload.data
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.instance_path,app.config['UPLOAD_FOLDER'],filename))
         # Get file data and save to your uploads folder
-
-        flash('File Saved', 'success')
-        return redirect(url_for('home'))
-
-    return render_template('upload.html')
+            flash('File Saved', 'success')
+            return redirect(url_for('home'))
+    return render_template('upload.html',form=form)
 
 
 @app.route('/login', methods=['POST', 'GET'])
